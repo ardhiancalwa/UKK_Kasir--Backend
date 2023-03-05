66a03825ec45c517cc5d7e0af00a73a106d60777
ardhiancalwa <ardhian_calwa_29rpl@student.smktelkom-mlg.sch.id>
2023-03-05 08:14:10 +0700

    kasir kafe

--- router/meja.js
+++ router/meja.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const meja = require("../models/index").meja
+const auth = require("../auth")
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get("/", async (req, res) => {
+app.get("/", auth, async (req, res) => {
   meja.findAll()
     .then(result => {
       res.json({
@@ -19,7 +20,7 @@ app.get("/", async (req, res) => {
     })
 })
 
-app.get("/:id", async (req, res) => {
+app.get("/:id", auth, async (req, res) => {
   let param = {
     id_meja: req.params.id
   }
@@ -36,9 +37,9 @@ app.get("/:id", async (req, res) => {
     })
 })
 
-app.get("/status/:status", async (req, res) => {
+app.get("/status/:status_meja", async (req, res) => {
   let param = {
-    status_meja: req.params.status
+    status_meja: req.params.status_meja
   }
   meja.findAll({ where: param })
     .then(result => {
@@ -53,7 +54,7 @@ app.get("/status/:status", async (req, res) => {
     })
 })
 
-app.post("/", async (req, res) => {
+app.post("/", auth, async (req, res) => {
   let data = {
     nomor_meja: req.body.nomor_meja,
     status_meja: req.body.status_meja
@@ -72,7 +73,7 @@ app.post("/", async (req, res) => {
     })
 })
 
-app.put("/", async (req, res) => {
+app.put("/", auth, async (req, res) => {
   let param = {
     id_meja: req.body.id_meja
   }
--- router/pemesanan.js
+++ router/pemesanan.js
@@ -1,13 +1,27 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql")
 const moment = require("moment")
 const transaksi = require("../models/index").transaksi
 const detail_transaksi = require("../models/index").detail_transaksi
+const menu = require("../models/index").menu
+const meja = require("../models/index").meja
+const user = require("../models/index").user
+
+const auth = require("../auth")
+const SECRET_KEY = "INIPUNYAKASIR"
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get("/detail", async (req, res) => {
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "kasir_kafe",
+  });
+
+app.get("/detail",auth, async (req, res) => {
     detail_transaksi.findAll({
         include: ["transaksi", "menu"]
     })
@@ -23,7 +37,7 @@ app.get("/detail", async (req, res) => {
         })
 })
 
-app.get("/detail/:id", async (req, res) => {
+app.get("/detail/:id",auth, async (req, res) => {
     let param = {
         id_transaksi: req.params.id
     }
@@ -43,7 +57,27 @@ app.get("/detail/:id", async (req, res) => {
         })
 })
 
-app.get("/", async (req, res) => {
+app.get("/detail/menu/:id",auth, async (req, res) => {
+    let param = {
+        id_menu: req.params.id
+    }
+    detail_transaksi.findAll({
+        include: ["transaksi", "menu"],
+        where: param
+    })
+        .then(result => {
+            res.json({
+                data: result
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: error.message
+            })
+        })
+})
+
+app.get("/",auth, async (req, res) => {
     transaksi.findAll({
         include: ["user", "meja"]
     })
@@ -59,7 +93,40 @@ app.get("/", async (req, res) => {
         })
 })
 
-app.get("/:id", async (req, res) => {
+app.get("/riwayat/:status/:id",auth, async (req, res) => {
+    let param = {
+        status: req.params.status,
+        id_user: req.params.id
+    }
+    transaksi.findAll({
+        include: ["user", "meja"],
+        where: param
+    })
+        .then(result => {
+            res.json({
+                data: result
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: error.message
+            })
+        })
+})
+app.get("/qtybymenu",auth, async (req, res) => {
+    conn.query(
+      "SELECT menu.id_menu, menu.nama_menu, SUM(detail_transaksi.qty) AS total_qty FROM menu JOIN detail_transaksi ON menu.id_menu = detail_transaksi.id_menu GROUP BY menu.id_menu",
+      (err, results, fields) => {
+        if (!err) {
+          res.send(results);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+
+app.get("/:id",auth, async (req, res) => {
     let param = {
         id_transaksi: req.params.id
     }
@@ -79,7 +146,7 @@ app.get("/:id", async (req, res) => {
         })
 })
 
-app.post("/", async (req, res) => {
+app.post("/",auth, async (req, res) => {
     let data_transaksi = {
         tgl_transaksi: moment().format("YYYY-MM-DD"),
         id_user: req.body.id_user,
@@ -87,7 +154,7 @@ app.post("/", async (req, res) => {
         nama_pelanggan: req.body.nama_pelanggan,
         status: req.body.status,
         jenis_pesanan: req.body.jenis_pesanan
-    }
+    }  
     transaksi.create(data_transaksi)
         .then(result => {
             let lastID = result.id_transaksi
@@ -114,9 +181,9 @@ app.post("/", async (req, res) => {
         })
 })
 
-app.put("/:id", async (req, res) => {
-    let param = { 
-        id_transaksi: req.params.id
+app.put("/",auth, async (req, res) => {
+    let param = {
+        id_transaksi: req.body.id_transaksi
     }
     let data = {
         status: req.body.status
@@ -134,7 +201,7 @@ app.put("/:id", async (req, res) => {
         })
 })
 
-app.delete("/:id", async (req, res) => {
+app.delete("/:id",auth,  async (req, res) => {
     let param = {
         id_transaksi: req.params.id
     }
